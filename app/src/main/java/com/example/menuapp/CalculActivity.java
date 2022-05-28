20ab68109fd517b3d5b9ebd89c2f8321769e54d8
epi edu <epi.edu@Epi.Etudiant>
2022-05-28 14:53:13 +0200

    factoriel

--- app/src/main/java/com/example/menuapp/CalculActivity.java
+++ app/src/main/java/com/example/menuapp/CalculActivity.java
@@ -3,11 +3,15 @@ package com.example.menuapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CalculActivity extends AppCompatActivity {
 
-    TextView tvNom, tvN;
+    TextView tvNom, tvN, tvFact;
+    Button btnCalcul;
+    int n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,20 @@ public class CalculActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calcul);
         tvNom = findViewById(R.id.nom);
         tvN = findViewById(R.id.n);
+        tvFact = findViewById(R.id.tvFact);
+        btnCalcul = findViewById(R.id.btnCalcul);
         Bundle bundle =getIntent().getExtras();
         String nom = bundle.getString("keyNom");
-        int n = bundle.getInt("keyN");
+        n = bundle.getInt("keyN");
         tvNom.setText(nom);
         tvN.setText(String.valueOf(n));
+    }
+
+    public void calculFact(View view) {
+        int fact = 1;
+        for (int i = 2; i < n; i++) {
+            fact*=i;
+        }
+        tvFact.setText(String.valueOf(fact));
     }
 }
