9e95db152f0e7be73975f5f6333784fe71da90a8
epi edu <epi.edu@Epi.Etudiant>
2022-05-28 15:08:30 +0200

    return

--- app/src/main/java/com/example/menuapp/CalculActivity.java
+++ app/src/main/java/com/example/menuapp/CalculActivity.java
@@ -2,6 +2,7 @@ package com.example.menuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,5 +35,17 @@ public class CalculActivity extends AppCompatActivity {
             fact*=i;
         }
         tvFact.setText(String.valueOf(fact));
+    }
+
+    public void okClick(View view) {
+        Intent intentRes = new Intent();
+        intentRes.putExtra("keyRes", tvFact.getText().toString());
+        setResult(RESULT_OK, intentRes);
+        CalculActivity.this.finish();
+    }
+
+    public void Annuler(View view) {
+        setResult(RESULT_CANCELED);
+        CalculActivity.this.finish();
     }
 }
